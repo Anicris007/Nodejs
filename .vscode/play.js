7e2ce449cef162bbf2be33f7263430feb29b5c0c
Anirudh Katkar <anikatkar007@gmail.com>
2023-12-24 15:00:52 +0530

    Done

---
+++ .vscode/play.js
@@ -0,0 +1,11 @@
+var name = "Anirudh";
+var age = 28;
+var hashobbies = true;
+
+function summarizeUser(userName, userAge, userHobbies)
+{
+    return
+        'Name is ' + userName + ' is = ' + userAge + 'and has hobbies.' + userHobbies  ;
+}
+
+console.log(summarizeUser(name, age, hashobbies));
